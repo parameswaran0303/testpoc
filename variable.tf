549216d58a5891675f61268b02ec26f4955307f6
parameswaran0303 <53178010+parameswaran0303@users.noreply.github.com>
2019-07-24 17:41:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
-variable "azure_client_id" {
-description = "Client ID for the Service Principal."
-  default = "1b24cfd9-c3fc-4af8-8f9f-b337ad05c881"
+variable "azure_client_id" {
+description = "Client ID for the Service Principal."
+default = "1b24cfd9-c3fc-4af8-8f9f-b337ad05c881"
 }
-variable "azure_client_secret" {
-description = "Client Secret for the Service Principal."
-  default = "45996867-90e7-4750-85dc-85fbdedd1081"
+variable "azure_client_secret" {
+description = "Client Secret for the Service Principal."
+default = "45996867-90e7-4750-85dc-85fbdedd1081"
 }
